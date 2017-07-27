e579cb307e7f521e69f206099831708a6fbbcb5b
Enhao <enhao.mobile@gmail.com>
2017-07-27 17:21:47 +0800

    feat: implement search bar
    * implement search bar
    * add json-loader
    * load card information from json
    * update card information

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,31 +1,42 @@
 <template>
   <div class="card-list">
     <div class="m-2">
-      <b-form-input class="mb-2" type="text" placeholder="Search"></b-form-input>
+      <b-form-input v-model="keyword" class="mb-2" type="text" placeholder="Search"></b-form-input>
       <div class="card-columns">
-        <b-card title="Bang!" sub-title="Bang!" class="mb-2">
-          被「Bang!」攻擊的玩家需要以有「射失」效果的牌抵銷，或被扣除一點生命值，在沒有裝備和特殊能力指定距離下，玩家只可攻擊距離為1的對手。
-        </b-card>
-        <b-card title="射失" sub-title="Missed!" class="mb-2">
-          被「Bang!」效果攻擊時用作抵銷「Bang!」。
-        </b-card>
-        <b-card title="啤酒" sub-title="Beer" class="mb-2">
-          回復一點生命值，在失去最後一點生命值時也可以使用。在剩餘兩人時無效果。
-        </b-card>
-        <b-card title="驚嚎" sub-title="Cat Balou" class="mb-2">
-          棄掉任何一位玩家的隨機一張手牌或者面前的任一張牌。
-        </b-card>
-        <b-card title="恐慌" sub-title="Panic!" class="mb-2">
-          從離自己距離1的一位玩家手上隨機抽去或面前中抽去一張牌加入手牌。
-        </b-card>
+        <div v-for="card in filteredCards">
+          <b-card :title="card.name.zh_tw" :sub-title="card.name.en_us" class="mb-2">
+            {{ card.description }}
+          </b-card>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import details from '../banginfo.json'
+
 export default {
-  name: 'card-list'
+  name: 'card-list',
+  data () {
+    return {
+      cards: [],
+      keyword: ''
+    }
+  },
+  mounted () {
+    this.cards = details.playingCards
+  },
+  computed: {
+    filteredCards: function () {
+      let self = this
+      return self.cards.filter(function (card) {
+        let keyword = self.keyword.toLowerCase()
+        return card.name.en_us.toLowerCase().indexOf(keyword) !== -1 ||
+          card.name.zh_tw.toLowerCase().indexOf(keyword) !== -1
+      })
+    }
+  }
 }
 </script>
 
